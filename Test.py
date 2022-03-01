a12c8b87de7faaa4a842bf5212e4d5dd4b56213b
finabe11 <finabe11@kingshigh.school.nz>
2022-03-01 14:57:25 +1300

    Finished chapter 4

--- Test.py
+++ Test.py
@@ -19,10 +19,12 @@ print (5+10.0) # You can add and subtract a float to an integer
 print (10-5.0) 
 print (5/2) # When you divide two integers, it will converted it to a float
 print (5//2) # Integer division 
-print (5%2) #This will result in 1. The number two goes into the number five two times, with one remainder.
+print (5%2) #This will result in 1. 
+#The number two goes into the number five two times, with one remainder. 
 print ("Hello World  " * 5)
-
-
-
+print (str(55+65))
+print (int(1.22222222222))
+print (int(55.0))
+print(float(44+10))
 
 # 4.4 Exercises 
---
+++ datatypes.py
@@ -0,0 +1,4 @@
+# 4.4 Excercises
+print ("You're never gonna make it \nThere's no way that you make it \nYeah maybe you can fake it \nBut you're never gonna make it")
+print("\n")
+print (" A moment of pain is worth a lifetime of glory. ") #Pete said that from the movie Unbroken
