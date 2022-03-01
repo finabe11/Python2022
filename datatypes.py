5c00ae1dcce1985c9e70393772a69685d1f7bc6d
finabe11 <finabe11@kingshigh.school.nz>
2022-03-01 14:58:13 +1300

    4.4 Excericses

--- datatypes.py
+++ datatypes.py
@@ -2,3 +2,5 @@
 print ("You're never gonna make it \nThere's no way that you make it \nYeah maybe you can fake it \nBut you're never gonna make it")
 print("\n")
 print (" A moment of pain is worth a lifetime of glory. ") #Pete said that from the movie Unbroken
+print("\n")
+print("fghgh
