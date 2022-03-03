9d3f4d4b7ac55afae2917b6905d91b41627033e0
finabe11 <finabe11@kingshigh.school.nz>
2022-03-04 10:58:46 +1300

    Create variables.py
    finished 5.3 exercises

---
+++ variables.py
@@ -0,0 +1,66 @@
+#assigning variables
+max_speed = 50
+acceleration = 10
+player_name = "Hero" 
+gravity = 9.8
+print ("player_name")
+print (player_name)
+#5.3 Excercises
+#1
+player_lives = 3
+#2
+user_name = "I am the best"
+#3
+money_balance = 1000
+#4
+phone_number = ("0123 456 789")
+#5
+my_name = ("Finn")
+#6
+my_age = ("15")
+#7
+current_year = 2022
+#8
+print (my_name)
+#9
+print (my_age)
+#10
+print (current_year)
+#11
+my_surname = ("Abey")
+#12
+my_school = ("Kings high School")
+#13
+my_favourite_food = ("Pizza")
+#14
+animals_name = ("Rex")
+#15
+blocks_of_chocolate = 10
+#16
+players_score = 500
+#17
+players_highest_score = 1000
+#18
+player_laps_completed = 5
+#19
+player_ammunition_left = 18
+#20
+player_current_gear = 4
+#21
+player_current_speed = "200kph"
+#5.5
+name = "jack"
+name += " smith"
+print (name)
+#5.6 exercises 
+money = 200
+hat = 20 
+pants = 40
+belt = 15
+shoes = 60
+print (money)
+print ("buying a hat")
+print (money-hat)
+print ()
+
+
