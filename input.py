1acf0efea9720c407510e0d6c6672c162052de0e
finabe11 <finabe11@kingshigh.school.nz>
2022-03-08 13:25:19 +1300

    6.3 exercises

---
+++ input.py
@@ -0,0 +1,41 @@
+#6.3 Exercises
+#1
+print("hello")
+input ("Push any key and then press enter: \n ")
+print("")
+#2
+input("Type your name and the push enter: \n")
+print("")
+#3
+input("Type your age and the push enter: \n")
+print("")
+#4
+user_name = input("Type your name and the push enter: \n")
+print("")
+#5
+user_age = input("Type your age and the push enter: \n")
+print("")
+#6
+favourite_movie = input("Type your favourite movie and the push enter: \n")
+print("")
+#7
+name_a_book = input("Type the name of a book and the push enter: \n")
+print("")
+#8
+name_a_adjective = input("Type an adjective and the push enter: \n")
+print("")
+#9
+name_a_noun = input("Type a noun and the push enter: \n")
+print("")
+#10
+name_a_verb = input("Type an verb and the push enter: \n")
+print("")
+#11
+print (f"Welcome, {user_name}, you are {user_age} years old \n")
+print(f"Your favourite movie is {favourite_movie} \n")
+print(f"Random book name: {name_a_book} \n")
+print(f"Random adjective: {name_a_adjective} \n")
+print(f"Random noun: {name_a_noun} \n")
+print(f"Random verb: {name_a_verb} \n")
+print(f"Your favourite movie and book are {favourite_movie} and {name_a_book} \n")
+print(f"Random adjective, noun and verb: {name_a_adjective}, {name_a_noun} and {name_a_verb} \n")
