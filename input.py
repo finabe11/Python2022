b4b3495e91c78e487cddd3cda50e64c3a853b102
finabe11 <finabe11@kingshigh.school.nz>
2022-03-09 14:59:25 +1300

    Update input.py
    doing 6.3 exercises

--- input.py
+++ input.py
@@ -1,5 +1,6 @@
 #6.3 Exercises
 #1
+"""
 print("hello")
 input ("Push any key and then press enter: \n ")
 print("")
@@ -39,3 +40,31 @@ print(f"Random noun: {name_a_noun} \n")
 print(f"Random verb: {name_a_verb} \n")
 print(f"Your favourite movie and book are {favourite_movie} and {name_a_book} \n")
 print(f"Random adjective, noun and verb: {name_a_adjective}, {name_a_noun} and {name_a_verb} \n")
+
+#12
+user_age = int(input("How old are you? \n"))
+#13
+print (f"You will be {user_age + 10} in ten years")
+print("")
+#14
+print(f"You were born in the year {2021 - user_age} ")
+print("")
+#15
+user_apples = int(input("How many apples do you have? \n"))
+#16
+user_friends = int(input("How many friends do you have? \n"))
+#17
+print(f"If you share your apples with you friends they will get {user_apples / user_friends} apples each")
+print("")
+print(f"If you share your apples with you friends evenly there will be {user_apples % user_friends} apples left over")
+print("")
+"""
+#18
+user_pizza = int(input("How many pizza's do you want? \n"))
+#19
+user_people = int(input("How many people are you feeding? \n"))
+#20
+print(f"If you share your pizza's with you friends they will get {user_pizza * 8 / user_people} slices each")
+print("")
+print(f"If you share your pizza's with you friends evenly there will be {user_pizza % user_people} slices left over")
+
