c54f295540eaf04e9d80453830ea5e05955a7625
finabe11 <finabe11@kingshigh.school.nz>
2022-03-16 13:22:22 +1300

    Create text_adventure_game.py

---
+++ text_adventure_game.py
@@ -0,0 +1,20 @@
+from adventurelib import *
+
+@when("brush teeth")
+@when("brush")
+@when("clean teeth")
+def brush_teeth():
+	print("You brush your teeth")
+
+
+
+
+
+
+
+
+def main():
+	start()
+
+if __name__ == '__main__':
+	main()
