ec6a0b5acaf3d399e8aff8d59831f0b6ee776769
finabe11 <finabe11@kingshigh.school.nz>
2022-04-01 12:23:57 +1300

    Update game.py
    Finally started my game

--- game.py
+++ game.py
@@ -1,1 +1,29 @@
-print("hello world")
+from adventurelib import *
+Room.items = Bag()
+
+
+#Rooms 
+starting_beach = Room("""
+	You look around and see a gently sloping hill leading up to the edge of the forest covered in godlen coloured sand,
+	you can feel the sand between your toes, the forest is just up ahead with massive plam trees towering above some small srubs,
+	you see that after the plam trees the forest get small and more thick. You heard some weird calling that sounds human, """)
+
+Path = Room("""
+	You start walking down the path, you see movement farther down the path, you see a bush that looks great for hiding in.""")
+
+Bush = Room("""
+	""")
+
+
+
+
+print("backstory")
+
+
+
+
+def main():
+	start()
+
+if __name__ == '__main__':
+	main()
