92b5e89b25ed7b4598de5c185e984fe72d574360
finabe11 <finabe11@kingshigh.school.nz>
2022-04-11 12:21:21 +1200

    Update game.py

--- game.py
+++ game.py
@@ -46,10 +46,17 @@ print(""" You are Francis Drake.
 	You are taking a holiday form being an explorer, you have decied to take a cruise, you are out on the side of the cruise at night looking at the ocean, you heard yelling,
 	you turn to the front of the boat and see a very vage shape in front of the boat, you realse is a large rock sticking out of the water, the boat is heading striaght for it, you brace for impact by lying down,
 	theres a tremendous bang, the whole boat pitches forwards and you are throwen off the boat, into the ptich black sea. 
-  you swim up to the surface and  """)
+  you swim up to the surface and  start treading water, while you have a look around to see where you are, it is very hard to see around, all you see is darkness, something bums into you you look at it is a briefcase, you grab on to it.
+  A cloud finishes passing over the moon, and now it is light so you have a look around to see if you can see the boat, you can't, but in the distance you see something else, you start swimming to it, while holding on to the briefcase.
+  As you get closer you realise that it's a island, you have another look around to see if you can see the cruise you can't, so you carry on swimming to the island.
+  The waves start to pick up, it is now very hard to see the island, you try and swim harder but that only make you more tired, you try one last desperate attempt to make it to the island, you feel your body going numb and you eyes slowing closing, you think that this is it, your whole life gone in an second.
+  You feel your consciousness stir, you feel something in your eyes, it's sand, you quicky open your eyes and sit up resiling that you are alive, you look  around, you are on the island you saw in the distance, you look down and see the breifcase next to you. """)
+print("")
+#the commands so people know how to play
+print("The commands you can use are go north,south,east and west, grab/take, use and inventory (inv)")
+print("")
 
 #connections for Rooms
-current_room = starting_beach
 starting_beach.east = Path 
 Path.west = Bush 
 Path.north = cannibal_village
@@ -62,6 +69,87 @@ Forest.west = Second_beach
 Forest.north = Death_forest
 
 #define and describe Items
+
+petrol = Item("petrol")
+petrol.description = "A yellowly liquid that might be useful for a vehicle"
+
+key = Item("key")
+key.descritption = "A sliver key, thats a little bit rusty, might open, unlock or start something"
+
+machete = Item("machete")
+machete.descritption = "A long sliver with patch of brown ruch and the handle is slowly crumbing off, might be useful for getting around a forest and defending yourself"
+
+
+
+#add items to bags
+starting_beach.items.add(machete)
+Second_beach.items.add(petrol)
+Cannibal_bosses_house.items.add(key)
+
+#define variable
+current_room = starting_beach
+inventory = Bag()
+print(current_room)
+
+#binds
+@when("go DIRECTION")
+def travel(direction):
+  global current_room
+  if direction in current_room.exits():
+    current_room = current_room.exit(direction)
+    print(f"You go {direction}.")
+    print(current_room)
+    print(current_room.exits())
+
+@when("look")
+def look():
+	print(current_room)
+	print(f"There are exits to the {current_room.exits()}.")
+	if len(current_room.items) > 0:     #if there are some items in the room
+		print("You also see:")
+		for item in current_room.items:
+			print(item)      #print out each item
+
+
+@when("take ITEM")
+@when("grab ITEM")
+@when("pick up ITEM")
+
+
+@when("get ITEM")
+def pickup(item):
+	if item in current_room.items:
+		t = current_room.items.take(item)
+		inventory.add(t)
+		print(f"You pick up the {item}")
+	else:
+		print(f"You don't see a {item}")
+
+
+@when("inventory")
+@when("inv")
+@when("show inventory")
+@when("bag")
+@when("what is in my pocket")
+@when("what is in my bag")
+def player_inventory():
+	print("You are carrying")
+	for item in inventory:
+		print(item)
+	
+
+@when("look at ITEM")
+def look_at(item):
+	if item in inventory:
+		t = inventory.find(item)
+		print(t.description)
+	else:
+		print(f"You aren't carrying an {item}")
+
+
+
+#binds
+
 
 
 def main():
