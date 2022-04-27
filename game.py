1c24e68add442f71f0fa6bc6adf71e135b62d717
finabe11 <finabe11@kingshigh.school.nz>
2022-04-27 15:55:45 +1200

    Update game.py
    game

--- game.py
+++ game.py
@@ -4,59 +4,58 @@ Room.items = Bag()
 
 #Rooms 
 starting_beach = Room("""
-	You look around and see a gently sloping hill leading up to the edge of the forest covered in godlen coloured sand,
-	you can feel the sand between your toes, the forest is just up ahead with massive plam trees towering above some small srubs,
-	you see that after the plam trees the forest get small and more thick. You heard some weird calling that sounds human, """)
+	You look around and see a gently sloping hill leading up to the edge of the forest covered in golden coloured sand.
+	You can feel the sand between your toes. The forest is just up ahead with massive plam trees towering above some small shrubs.
+	You see that after the palm trees the forest get small and more thick. You heard some weird calling that sounds human. You look around and see something sticking up in the ground, it looks like a weapon of some sort.""")
 
 Path = Room("""
-	You start walking down the path, with trees towdering over you, you stand on a tiwg, is snaps giving you am fright, then you see movement farther down the path, you see a bush that looks great for hiding in.""")
+	You start walking down the path, with trees towering over you. You stand on a twig, it snaps giving you a fright. Then you see movement farther down the path. You see a bush that looks great for hiding in.""")
 
 Bush = Room("""
-	You can just see out of the bush you see some people with cloths around there wasts carrying spears and sheilds, you reasles
-	that they are cannibals, """)
+	You can just see out of the bush. You see some people with clothes around their waists carrying spears and shields. You realise
+	that they are cannibals. """)
 
-cannibal_village = Room(""" You look ahead and see some things resembling houses, you walk forward to look closer,
-	you see that the house are made with stick stuck together with dried mud then plasered with leaves in some areas,
-	you look up there are heaps of these houses, with looks like a massive farther away,
-	while you are looking around you see another path""")
+cannibal_village = Room(""" You look ahead and see some things resembling houses. You walk forward to look closer.
+	You see that the house are made with sticks stuck together with dried mud then plastered with leaves in some areas.
+	You look up, there are a lot more similar houses further ahead. There's another house, but it's massive and further away than the rest. While you are looking around you see another path.""")
 
-Cannibal_bosses_house = Room(""" You start walking towards the large house to see if you can find anything, something comes out of the big hut, it is a very strong looking cannibals, 
-	it starts running towards you """)
+Cannibal_bosses_house = Room(""" You start walking towards the large house to see if you can find anything. Something comes out of the big hut. It is a very strong looking cannibal.
+	It starts running towards you. It has something shinny tied aroung its neck.""")
 
 second_path = Room(""" 
-	You see another path that looks similar to the one you came through the beach. """)
+	You see another path that looks similar to the one you came through from the beach. """)
 
 Third_beach = Room("""
-	Through the trees you glimpse something blue, you brust out of the trees to find a beach slowly sloping down, with the sea about 100 meters away,
-	 to spot something farther down the beach, its a jetty with a boat at the end of it, you start running.""")
+	Through the trees you glimpse something blue. You burst out of the trees to find a another beach slowly sloping down, with the sea about 100 meters away.
+	You spot something farther down the beach. It's a jetty with a boat at the end of it. You start running.""")
 
 Forest = Room("""
-	You have decided that it would be safer to go throught the forest, you have a hard time going throught the forest,
+	You have decided that it would be safer to go throught the forest. You have a hard time cutting throught the forest,
 	with all it's vines and branches.""")
 
 Second_beach = Room("""
-	You stumble out of the forest on to a sand beach covered in shells, with no idea of which way you have come. >you have something glithing or food or have a storage thing from the cannibals""")
+	You stumble out of the forest on to a sandy beach covered in shells, with no idea of which way you have come. You look down the beach and see a small hut, mabye made by another traveler. You walk towards it and see inside that there  is a can of some sort. """)
 
 Death_forest = Room("""
-	You start wayding throught the forest againg and you sand on something slipery and you fall heaverly and then you feel something bit you it very painful you look doen and see a snake,
-	you try and move but you are prazlyed, you died a slow and painful death.""")
+	You start wading through the forest again and you stand on something slippery. You fall heavily and then you feel something bite you. It's very painful. You look down and see a snake.
+	You try and move but you are paralysed. You die a slow and painful death.""")
 
 #this is my back ground story
 print(""" You are Francis Drake.
-	You are taking a holiday form being an explorer, you have decied to take a cruise, you are out on the side of the cruise at night looking at the ocean, you heard yelling,
-	you turn to the front of the boat and see a very vage shape in front of the boat, you realse is a large rock sticking out of the water, the boat is heading striaght for it, you brace for impact by lying down,
-	theres a tremendous bang, the whole boat pitches forwards and you are throwen off the boat, into the ptich black sea. 
-  you swim up to the surface and  start treading water, while you have a look around to see where you are, it is very hard to see around, all you see is darkness, something bums into you you look at it is a briefcase, you grab on to it.
-  A cloud finishes passing over the moon, and now it is light so you have a look around to see if you can see the boat, you can't, but in the distance you see something else, you start swimming to it, while holding on to the briefcase.
-  As you get closer you realise that it's a island, you have another look around to see if you can see the cruise you can't, so you carry on swimming to the island.
-  The waves start to pick up, it is now very hard to see the island, you try and swim harder but that only make you more tired, you try one last desperate attempt to make it to the island, you feel your body going numb and you eyes slowing closing, you think that this is it, your whole life gone in an second.
-  You feel your consciousness stir, you feel something in your eyes, it's sand, you quicky open your eyes and sit up resiling that you are alive, you look  around, you are on the island you saw in the distance, you look down and see the breifcase next to you. """)
-print("")
+	You are taking a holiday from being an explorer. You have decided to take a cruise. You are out on the side of the cruise at night looking at the ocean. You hear yelling.
+	You turn to the front of the boat and see a very vague shape in front of the boat. You realise it is a large rock sticking out of the water. The boat is heading striaght for it. You brace for impact by lying down.
+	There's a tremendous bang. The whole boat pitches forwards and you are thrown off the boat into the pitch black sea. 
+  You swim up to the surface and start treading water while you have a look around to see where you are. It is very hard to see around, all you see is darkness. Something bumps into you. You look at it. It's a briefcase. You grab onto it.
+  A cloud finishes passing over the moon. Now it's lighter so you have a look around to see if you can see the boat. You can't, but in the distance you see something else. You start swimming to it, while holding onto the briefcase.
+  As you get closer you realise that it's a island. You have another look around to see if you can see the cruise ship. You can't, so you carry on swimming to the island.
+  The waves start to pick up. It is now very hard to see the island. You try and swim harder but that only makes you more tired. You try one last desperate attempt to make it to the island. You feel your body going numb and your eyes slowly closing. You think that this is it, your whole life gone in a second.
+  You feel your consciousness stir. You feel something in your eyes, it's sand. You quicky open your eyes and sit up realising that you are alive. You look around. You are on the island you saw in the distance. You look down and see the briefcase next to you.""")
+
 #the commands so people know how to play
-print("The commands you can use are go north,south,east and west, grab/take, use and inventory (inv)")
-print("")
+print("The commands you can use are look around (look), go north,south,east and west, grab/take, use and inventory (inv). You type the command in and then press enter")
 
 #connections for Rooms
+current_room = starting_beach
 starting_beach.east = Path 
 Path.west = Bush 
 Path.north = cannibal_village
@@ -68,20 +67,24 @@ Bush.west = Forest
 Forest.west = Second_beach
 Forest.north = Death_forest
 
-#define and describe Items
+#define Items 
+Item.description = ""   #This adds a blank description to each item 
 
 petrol = Item("petrol")
-petrol.description = "A yellowly liquid that might be useful for a vehicle"
+petrol.description = "A yellowy liquid that might be useful for a vehicle"
 
 key = Item("key")
-key.descritption = "A sliver key, thats a little bit rusty, might open, unlock or start something"
+key.descritption = "A silver key, that's a little bit rusty, might open, unlock or start something"
 
 machete = Item("machete")
-machete.descritption = "A long sliver with patch of brown ruch and the handle is slowly crumbing off, might be useful for getting around a forest and defending yourself"
+machete.descritption = "A long silver with patches of brown rust, the handle is slowly crumbling off, might be useful for getting around a forest and defending yourself"
 
+briefcase = Item("briefcase")
+briefcase.descritption = "A briefcase full of money, might be useful if you get off the island"
 
 
 #add items to bags
+starting_beach.items.add(briefcase)
 starting_beach.items.add(machete)
 Second_beach.items.add(petrol)
 Cannibal_bosses_house.items.add(key)
@@ -90,16 +93,36 @@ Cannibal_bosses_house.items.add(key)
 current_room = starting_beach
 inventory = Bag()
 print(current_room)
+enter = False
+boss = True 
+
 
 #binds
+
 @when("go DIRECTION")
 def travel(direction):
   global current_room
+  global enter
+
+  if current_room == starting_beach and inventory.find("machete") == None:
+  	print("You need something to cut into the forest")
+  	return
+  if current_room == starting_beach and inventory.find("machete") and enter == False:
+  	print("You use the machete to slash a path through the forest")
+  	enter = True 
+  	return
+
+
+
+ 
   if direction in current_room.exits():
     current_room = current_room.exit(direction)
     print(f"You go {direction}.")
     print(current_room)
     print(current_room.exits())
+    if current_room == Death_forest:
+    	quit()
+
 
 @when("look")
 def look():
@@ -114,14 +137,13 @@ def look():
 @when("take ITEM")
 @when("grab ITEM")
 @when("pick up ITEM")
-
-
 @when("get ITEM")
 def pickup(item):
 	if item in current_room.items:
 		t = current_room.items.take(item)
 		inventory.add(t)
 		print(f"You pick up the {item}")
+
 	else:
 		print(f"You don't see a {item}")
 
@@ -148,7 +170,14 @@ def look_at(item):
 
 
 
-#binds
+
+
+@when("kill boss")
+@when("fight boss")
+def fight_boss():
+	if inventory.find("machete") and current_room == Cannibal_bosses_house:
+		print("print your fight ........ Boss drops item")
+
 
 
 
